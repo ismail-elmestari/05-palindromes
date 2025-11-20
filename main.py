2a1aec87010945374dde0b415eedaae82517d4e3
ismail-elmestari <ismail.elmestari@edu.esiee.fr>
2025-11-20 10:11:40 +0000

    05-palindromes

--- main.py
+++ main.py
@@ -1,21 +1,54 @@
 #### Fonction secondaire
+"""
+Script contenant la fonction ispalindrome et un test de différents mots.
 
+"""
 
 def ispalindrome(p):
+    """
+    Verifie si la chaine de caractère en paramètre est un palyndrome
+    """
 
-    # votre code ici
-    
+    p=p.lower()
+    p=p.replace(" ","")
+
+    ponctu=".,!?_-:'"
+    for c in ponctu :
+        if c in p :
+            p=p.replace(c,"")
+
+    car_spec="àéèêëùôç"
+    car_norm="aeeeeuoc"
+
+    for c in car_spec :
+        i=car_spec.index(c)
+        if c in p :
+            p=p.replace(c,car_norm[i])
+
+
+
+
+    if p==p[::-1] :
+        return True
+
+
     return False
 
 #### Fonction principale
 
 
+
 def main():
+
+    """
+    Test ispalindrome sur differents mots
+    """
 
     # vos appels à la fonction secondaire ici
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
+
 
 
 if __name__ == "__main__":
